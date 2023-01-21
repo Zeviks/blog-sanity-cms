8c677abd5f2548a42b4e9af69f0815b288dc2a15
Natanael Pacheco <natepacheco716@gmail.com>
2023-01-20 18:42:22 -0600

    Complete Initial Setup

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>This is the main page........</h1>
+      <p>This is a simple description...</p>
+    </div>
+  );
+};
+
+export default HomePage;
